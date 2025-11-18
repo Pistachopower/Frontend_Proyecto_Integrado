ea520da48af84465cb47a4079c8084e9fd2717ba
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2025-11-18 21:08:40 +0100

    comunicacion con backend

---
+++ src/services/listarProductos.js
@@ -0,0 +1,13 @@
+export function listarProductos() {
+    return fetch('http://127.0.0.1:8000/api/v1/productos')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Problemas de conexion ' + response.statusText);
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.error('Hay un problema. Este es el error:', error);
+            throw error;
+        });
+}
