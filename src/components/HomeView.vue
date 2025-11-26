8fa90c0ac47588a792f850d4e867be9d1e3c7130
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2025-11-26 20:27:05 +0100

    se hace la estructura del frontend

---
+++ src/components/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+const counter = useCounterStore()
+</script>
+
+
+<template>
+  <div>
+    <h2>Productos</h2>
+
+  </div>
+
+  <div>
+    <h2>Contador: {{ counter.count }}</h2>
+    <button @click="counter.increment()">Incrementar</button>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
